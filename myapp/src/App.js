08ffed93dff34f7b7f22651b770c99bf98d8d193
Neha S Eluvathingal <nehasajie@gmail.com>
2024-09-29 19:55:32 +0530

    javascript file

---
+++ myapp/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+
+document.getElementById('click').addEventListener('click', function() {
+  // Add form validation logic here if necessary
+
+  // Redirect to another page
+  window.location.href = 'upload.html'; // Make sure this file exists
+});
+
+function App() {
+  return (
+    <div>
+       <div class="logo">
+    <img src="song.png" alt="song to Music"/>
+    <h1>Songs2Notes</h1>
+</div>
+
+<div class="container">
+    <h1>Sign In</h1>
+    <div class="form">
+        <input type="email" placeholder="Email or phone number"/>
+        <input type="password" placeholder="Password"/>
+        <input type="submit" value="Sign In" id="click"/>
+    </div>
+    <div class="content">
+        <h2>New to Music app? <a href="#">Sign up now.</a> </h2> 
+    </div>
+</div>
+    </div>
+   
+  );
+}
+
+export default App;
